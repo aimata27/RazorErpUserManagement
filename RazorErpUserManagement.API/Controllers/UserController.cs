40c449f728f7610a5f0f4a2e05c4126bc77a2b26
aimata27 <alvarez.aiman@gmail.com>
2024-08-10 14:58:31 +0800

    Removed DB Assembly

--- RazorErpUserManagement.API/Controllers/UserController.cs
+++ RazorErpUserManagement.API/Controllers/UserController.cs
@@ -7,5 +7,9 @@ namespace RazorErpUserManagement.API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        public UserController()
+        {
+                
+        }
     }
 }
--- RazorErpUserManagement.DB/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RazorErpUserManagement.DB
-{
-    public class Class1
-    {
-
-    }
-}
